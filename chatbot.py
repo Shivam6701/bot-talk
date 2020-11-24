b28e9a3f161d8db52507f2aaa769d4cc38041908
Shivam6701 <68462509+Shivam6701@users.noreply.github.com>
2020-11-24 15:58:17 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,11 @@
 from chatterbot_lib import Chatterbot_mod
-from pyttsx3_lib import Pyttsx3_mod
 
 class Bot:
     def __init__(self):
         super().__init__()
 
         self.c_m = Chatterbot_mod()
-        self.p_m = Pyttsx3_mod()
+        
 
     def question(self,qu):
 
@@ -19,16 +18,7 @@ class Bot:
                 answer = self.c_m.respond(ques)
                 print(answer)
                 return answer
-                #self.p_m.voice(answer)
-
-
+               
             else:
-
-                #self.p_m.voice("Thank you, Have a good day")
                 return "Thank you"
-                #break
-
-
-
-
-
+                
