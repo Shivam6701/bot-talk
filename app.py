c640bfc2cc98c0a27da255b050a81432a3590c60
Shivam6701 <68462509+Shivam6701@users.noreply.github.com>
2020-11-24 14:49:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from flask import Flask , render_template, request
 from chatbot import Bot
-import os
-import waitress
 
 if __name__ == '__main__':
     b1 = Bot()
 
-
-
     app = Flask(__name__)
-
 
     @app.route('/')
     def index():
         return render_template('index.html')
-
-
     @app.route('/', methods = ['POST'])
     def abou():
         ques = request.form['box']
@@ -23,8 +16,5 @@ if __name__ == '__main__':
 
         return render_template('index.html', out = answer)
 
-
-
     app.run()
-    port = int(os.environ.get('PORT', 33507))
-    waitress.serve(app, port=port)
+   
