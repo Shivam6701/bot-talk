7816953968b6094a6db68b284b6f6a55cf4bf806
Shivam6701 <68462509+Shivam6701@users.noreply.github.com>
2020-11-24 19:42:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask , render_template, request
+from flask import Flask, render_template, request
+from chatbot import Bot
 
 app = Flask(__name__)
 
+
+b1 = Bot()
+
 @app.route('/')
-def index():    
+def index():
     return render_template('index.html')
-if __name__=="__main__":
+
+@app.route('/', methods=['POST'])
+def abou():
+    ques = request.form['box']
+    answer = b1.question(ques)
+    return render_template('index.html', out=answer)
+if __name__ == '__main__':
     app.run()
-   
